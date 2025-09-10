b8be32ea381455d0e035614c36bd8e1563889a67
josem441 <josemiguelceballosjaramillo@gmail.com>
2025-09-10 13:50:25 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,25 +1,29 @@
-const CACHE = "ade-map-v8-6-3";
+
+const CACHE_NAME = 'ade-map-v8.6.6';
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/maskable-512.png'
 ];
 
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
 });
 
-self.addEventListener("activate", (e) => {
+self.addEventListener('activate', (e) => {
   e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
+    caches.keys().then(keys => Promise.all(keys.map(k => k === CACHE_NAME ? null : caches.delete(k))))
   );
 });
 
-self.addEventListener("fetch", (e) => {
+self.addEventListener('fetch', (e) => {
   const url = new URL(e.request.url);
   if (url.origin === location.origin) {
     e.respondWith(
-      caches.match(e.request).then((res) => res || fetch(e.request))
+      caches.match(e.request).then(res => res || fetch(e.request))
     );
   }
 });
