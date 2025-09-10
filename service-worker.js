3ea7fdbd1ac1140dcb964b4efedf700d8d05e8c5
josem441 <josemiguelceballosjaramillo@gmail.com>
2025-09-10 13:16:30 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,25 @@
+const CACHE = "ade-map-v8-6-3";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+];
 
-const CACHE = 'ade-map-v8-1';
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll([
-    './','./index.html','./manifest.json','./icons/icon-180.png','./icons/icon-512.png',
-    'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
-    'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
-  ])));
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
 });
-self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', (e) => {
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
   const url = new URL(e.request.url);
   if (url.origin === location.origin) {
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-  } else {
-    e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
+    e.respondWith(
+      caches.match(e.request).then((res) => res || fetch(e.request))
+    );
   }
 });
